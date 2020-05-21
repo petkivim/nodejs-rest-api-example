87125549df60a3ec71336c91477ac682507c6f1d
Petteri Kivimäki <petteri.kivimaki@niis.org>
2020-05-21 07:15:24 +0300

    Log responses to console

--- server.js
+++ server.js
@@ -8,30 +8,35 @@ app.set("port", process.env.PORT || 4000);
 app.get('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
    var response = { "response" : "This is GET method." }
+   console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.get('/:id', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
    var response = { "response" : "This is GET method with id=" + req.params.id + "." }
+   console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.post('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
    var response = { "response" : "This is POST method." }
+   console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.put('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
    var response = { "response" : "This is PUT method." }
+   console.log(response);
    res.end(JSON.stringify(response));
 })
 
 app.delete('/', function (req, res) {
    res.writeHead(200, {'Content-Type': 'application/json'});
    var response = { "response" : "This is DELETE method." }
+   console.log(response);
    res.end(JSON.stringify(response));
 })
 
