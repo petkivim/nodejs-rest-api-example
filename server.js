ede33362f0b4b2570406aab637f64b25e310d8dd
petteri.kivimaki <petteri.kivimaki@niis.org>
2024-09-01 09:54:43 +0300

    Remove semicolons from code

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000)
 
 app.get('/', function (req, res) {
   console.log('GET request received')
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'})
   var response = { "response" : "This is GET method." }
-  console.log(response);
-  res.end(JSON.stringify(response));
+  console.log(response)
+  res.end(JSON.stringify(response))
 })
 
 app.get('/:id', function (req, res) {
   console.log('GET /:id request received')
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'})
   var response = { "response" : "This is GET method with id=" + req.params.id + "." }
-  console.log(response);
-  res.end(JSON.stringify(response));
+  console.log(response)
+  res.end(JSON.stringify(response))
 })
 
 app.post('/', function (req, res) {
   console.log('POST request received')
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'})
   var response = { "response" : "This is POST method." }
-  console.log(response);
-  res.end(JSON.stringify(response));
+  console.log(response)
+  res.end(JSON.stringify(response))
 })
 
 app.put('/', function (req, res) {
   console.log('PUT request received')
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'})
   var response = { "response" : "This is PUT method." }
-  console.log(response);
-  res.end(JSON.stringify(response));
+  console.log(response)
+  res.end(JSON.stringify(response))
 })
 
 app.delete('/', function (req, res) {
   console.log('DELETE request received')
-  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'})
   var response = { "response" : "This is DELETE method." }
-  console.log(response);
-  res.end(JSON.stringify(response));
+  console.log(response)
+  res.end(JSON.stringify(response))
 })
 
 const server = app.listen(app.get("port"), function () {
