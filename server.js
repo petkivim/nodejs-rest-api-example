fc5096844ac7d390d693b93c7426813a7713462b
petteri.kivimaki <petteri.kivimaki@niis.org>
2024-09-01 09:13:10 +0300

    Fix indentation and improve logging

--- server.js
+++ server.js
@@ -6,47 +6,50 @@ var app = express();
 app.set("port", process.env.PORT || 4000);
 
 app.get('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+  console.log('GET request received')
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is GET method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 app.get('/:id', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method with id=" + req.params.id + "." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+  console.log('GET /:id request received')
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is GET method with id=" + req.params.id + "." }
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 app.post('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is POST method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+  console.log('POST request received')
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is POST method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 app.put('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is PUT method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+  console.log('PUT request received')
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is PUT method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 app.delete('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is DELETE method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
+  console.log('DELETE request received')
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var response = { "response" : "This is DELETE method." }
+  console.log(response);
+  res.end(JSON.stringify(response));
 })
 
 const server = app.listen(app.get("port"), function () {
-
   const host = server.address().address
   const port = server.address().port
 
-  console.log("Node.js API app listening at http://%s:%s", host, port)
-
+  console.log("Node.js API app running at http://%s:%s", host, port)
 })
 
 process.on('SIGINT', () => {
